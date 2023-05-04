23acb6f0cea2ff1cdd892051ee33f1937ba479ae
trevormadaramoringaschool <trevor.madara@student.moringaschool.gcom>
2023-05-04 18:44:16 +0300

    DOne

--- src/components/HeroBanner/HeroBanner.jsx
+++ src/components/HeroBanner/HeroBanner.jsx
@@ -1,15 +1,20 @@
 import "../HeroBanner/HeroBanner.css";
-import React from 'react';
+import React, {useState} from 'react';
 
-function HeroBanner() {
+function HeroBanner({onSearch}) {
+    const [searchText, setSearchText] = useState('');
+
+    const handleSearch= (event) =>{
+        onSearch(searchText)
+    }
   return (
     <div className="hero-banner">
       <div className="hero-banner__overlay"></div>
       <div className="hero-banner__content">
         <h1 className="hero-banner__title">Welcome to My Movie App</h1>
         <p className="hero-banner__subtitle">Find your favorite movies and TV shows</p>
-        <form className="hero-banner__search-form">
-          <input type="text" placeholder="Search movies..." />
+        <form className="hero-banner__search-form" onSubmit={handleSearch}>
+          <input type="text" placeholder="Search movies..."  value={searchText} onChange={(event)=> setSearchText(event.target.value)}/>
           <button type="submit">Search</button>
         </form>
       </div>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,40 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import HeroBanner from "./HeroBanner/HeroBanner";
-function Home(){
-    return(
-        <div>
-            <HeroBanner />
-        </div>
-    )
-}
-export default Home;
+
+function Home() {
+    const [movies, setMovies] = useState([]);
+    const [searchResults, setSearchResults] = useState([]);
+  
+    useEffect(() => {
+      fetch('http://localhost:3000/movie')
+        .then((response) => response.json())
+        .then((data) => setMovies(data))
+        .catch((error) => console.log(error));
+    }, []);
+  
+    const handleSearch = (searchTerm) => {
+      const results = movies.filter((movie) =>
+        movie.title.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+      setSearchResults(results);
+    };
+  
+    return (
+      <div className="Home">
+        <HeroBanner
+          onSearch={handleSearch}
+        />
+        {searchResults.length > 0 ? (
+          <ul>
+            {searchResults.map((movie) => (
+              <li key={movie.id}>{movie.title}</li>
+            ))}
+          </ul>
+        ) : (
+         <>/</>
+        )}
+      </div>
+    );
+  }
+  
+  export default Home;
