8346c375285ff18731dd2c280704d29a6aab6cc8
trevormadaramoringaschool <trevor.madara@student.moringaschool.gcom>
2023-05-04 22:19:02 +0300

    Added Footer Component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./App.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="container">
+        <p>&copy; 2023 Trev Movies. All Rights Reserved.</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
