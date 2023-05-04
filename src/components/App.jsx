441e8915d2663c4091e94ac495f280ae0be375fd
trevormadaramoringaschool <trevor.madara@student.moringaschool.gcom>
2023-05-04 17:23:43 +0300

    Final App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 import { Routes, Route} from "react-router-dom";
 import Nav from './Nav';
@@ -7,12 +7,20 @@ import Movies from './Movies';
 import Login from './Login';
 
 function App() {
+    const[movies,setMovies] = useState([]);
+
+    useEffect(()=>{
+      fetch('http://localhost:3000/movie')
+      .then((response)=> response.json())
+      .then(data => setMovies(data))
+      .catch((error)=> console.error(error));
+    }, [])
   return (
     <div className="App">
       <Nav />
       <Routes>
         <Route  path='/' element={<Home />}/>
-        <Route  path='/movies' element={<Movies />}/>
+        <Route  path='/movies' element={<Movies movies={movies}/>}/>
         <Route path='/login' element={<Login />} />
       </Routes>
     </div>
