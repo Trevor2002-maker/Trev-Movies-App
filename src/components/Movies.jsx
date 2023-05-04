d198eff8e6f421a88f0fb0ada2df2f73b85bf17a
trevormadaramoringaschool <trevor.madara@student.moringaschool.gcom>
2023-05-05 02:17:17 +0300

    Used UseParams on Movies

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 import MovieItem from './MovieItem';
 import Footer from './Footer';
 
 function Movies() {
   const [movies, setMovies] = useState([]);
+  const {id} = useParams();
   const[currentPage, setCurrentPage] = useState(1);
   const moviesPerPage = 10;
 
@@ -12,12 +14,14 @@ function Movies() {
       .then((response) => response.json())
       .then((data) => setMovies(data))
       .catch((error) => console.log(error));
-  }, []);
+  }, [id]);
 
   const indexOfLastMovie = currentPage * moviesPerPage;
   const indexOfFirstMovie = indexOfLastMovie - moviesPerPage;
   const currentMovies = movies.slice(indexOfFirstMovie, indexOfLastMovie);
-
+  if(!movies){
+    return<div>Loading...</div>;
+  }
 
   return (
     <div className='Movies'>
