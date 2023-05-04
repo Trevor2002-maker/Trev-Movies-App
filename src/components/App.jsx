0af9ba97cf9e6cddffcf45254d9194faace82ad2
trevormadaramoringaschool <trevor.madara@student.moringaschool.gcom>
2023-05-04 16:42:24 +0300

    Finalised Nav Component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import Header from "./Header";
 import './App.css';
+import { Routes, Route} from "react-router-dom";
 import Nav from './Nav';
+import Home from './Home';
+import Movies from './Movies';
+import Login from './Login';
+
 function App() {
   return (
     <div className="App">
       <Header />
       <Nav />
+      <Routes>
+        <Route  path='/' element={<Home />}/>
+        <Route  path='/movies' element={<Movies />}/>
+        <Route path='/login' element={<Login />} />
+      </Routes>
     </div>
   );
 }
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,12 +3,14 @@ import "./App.css";
 import { NavLink } from "react-router-dom";
 import {RiMovieLine} from "react-icons/ri"
 import {FiHome} from "react-icons/fi";
+import {MdOutlineAccountCircle} from "react-icons/md";
 
 function Nav(){
     return(
         <div className="navbar">
             <NavLink to="/"><FiHome/>Home</NavLink>
-            <NavLink to="/movies"><RiMovieLine /></NavLink>
+            <NavLink to="/movies"><RiMovieLine />Movies</NavLink>
+            <NavLink to ="/login"><MdOutlineAccountCircle />Login</NavLink>
         </div>
     )
 }
