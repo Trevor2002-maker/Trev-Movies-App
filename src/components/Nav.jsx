a634acaba2cd4bd81bb717f61a7eb58ecf954a8e
trevormadaramoringaschool <trevor.madara@student.moringaschool.gcom>
2023-05-04 17:41:09 +0300

    New Commit

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,8 +2,6 @@ import React from "react";
 import "./App.css";
 import { NavLink } from "react-router-dom";
 import {RiMovieLine} from "react-icons/ri"
-import {FiHome} from "react-icons/fi";
-import {MdOutlineAccountCircle} from "react-icons/md";
 import logo from "./images/trevmovies.png";
 function Nav(){
     const linkStyles = {
@@ -13,9 +11,7 @@ function Nav(){
     return(
         <div className="navbar">
              <img alt="logo" src={logo} />
-            <NavLink to="/" exact style={linkStyles}><FiHome /> Home</NavLink>
             <NavLink to="/movies"exact style={linkStyles}><RiMovieLine /> Movies</NavLink>
-            <NavLink to ="/login"exact style={linkStyles}><MdOutlineAccountCircle /> Login</NavLink>
         </div>
     )
 }
